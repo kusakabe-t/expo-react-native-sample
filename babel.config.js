eae49d65914aa467fedcdc7e9183b8efa353d8d3
kusakabe <kusakabe.takuya@alleeks.com>
2023-01-01 01:47:47 +0900

    fix babelの設定を修正しました

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,29 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        // alias: { tests: ['./tests'], "@components": "./src/components" }
-      },
-    ],
+    "plugins": [
+      [
+        "module-resolver",
+        {
+          "root": ["."],
+          "alias": {
+            "~/*": "./src/*",
+          }
+        }
+      ]
+    ]
   }
 }
+
+// plugins: [
+//   [
+//     "module-resolver",
+//     {
+//       "root": ["./"],
+//       // "alias": {
+//       //   "~/*": "./src/*",
+//       // },
+//       // extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+//     }
+//   ],
+// ]
